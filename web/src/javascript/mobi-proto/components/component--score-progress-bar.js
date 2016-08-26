df35b387489b5fbf077ecc0d08a1592494d86d89
Matt Lickei <mlickei@vipasolutions.com>
2016-08-25 20:07:48 -0500

    Added new scores styling.

---
+++ web/src/javascript/mobi-proto/components/component--score-progress-bar.js
@@ -0,0 +1,23 @@
+jQuery(function () {
+
+    var $scoreBars = $('.score-bar-container');
+
+    function init() {
+        $scoreBars.each(function () {
+            var $this = $(this),
+                $barVal = $this.find('.score-bar-value'),
+                max = parseInt($barVal.data('score-bar-max')),
+                val = parseInt($barVal.data('score-bar-val')) || -1;
+
+            if (val > 0) {
+                $barVal.width(Math.ceil((max / val) * 100) + '%');
+            }
+            else {
+                $barVal.width('0%');
+            }
+        });
+    }
+
+    init();
+
+});
